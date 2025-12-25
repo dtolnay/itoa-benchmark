dac7b2a311a4cc7cf1dafa68b94371dbb98d85ad
David Tolnay <dtolnay@gmail.com>
2025-12-25 11:56:35 -0800

    Simplify zmij using to_be_bytes instead of pointer write

--- src/zmij.rs
+++ src/zmij.rs
@@ -1,6 +1,3 @@
-use std::mem::MaybeUninit;
-use std::slice;
-
 fn to_bcd8(abcdefgh: u32) -> u64 {
     // An optimization from Xiang JunBo.
     // Three steps BCD. Base 10000 -> base 100 -> base 10.
@@ -19,46 +16,23 @@ fn to_bcd8(abcdefgh: u32) -> u64 {
 
 const ZEROS: u64 = 0x30303030_30303030; // 0x30 == '0'
 
-#[repr(C, align(8))]
-struct AlignedBuffer([MaybeUninit<u8>; 16]);
-
 pub fn write_significand(value: u64, f: &dyn Fn(&str)) {
-    let mut buffer = AlignedBuffer([MaybeUninit::uninit(); 16]);
-    let out = buffer.0.as_mut_ptr().cast::<u64>();
-
     if value < 100_000_000 {
         let bcd = to_bcd8(value as u32);
         let leading_zeros = (bcd | 1).leading_zeros() as usize / 8;
-        unsafe {
-            *out = bcd.to_be() | ZEROS;
-        }
-        f(unsafe {
-            str::from_utf8_unchecked(slice::from_raw_parts(
-                buffer.0.as_ptr().cast::<u8>().add(leading_zeros),
-                8 - leading_zeros,
-            ))
-        });
+        let bytes = (bcd | ZEROS).to_be_bytes();
+        f(unsafe { str::from_utf8_unchecked(&bytes[leading_zeros..]) });
     } else if value < 10_000_000_000_000_000 {
         let bbccddee = (value / 100_000_000) as u32;
         let ffgghhii = (value % 100_000_000) as u32;
-        let bcd = to_bcd8(bbccddee);
-        let mut leading_zeros = bcd.leading_zeros() as usize / 8;
-        unsafe {
-            *out = bcd.to_be() | ZEROS;
-        }
-        let bcd = to_bcd8(ffgghhii);
-        if leading_zeros == 8 {
-            leading_zeros += bcd.leading_zeros() as usize / 8;
-        }
-        unsafe {
-            *out.add(1) = bcd.to_be() | ZEROS;
-        }
-        f(unsafe {
-            str::from_utf8_unchecked(slice::from_raw_parts(
-                buffer.0.as_ptr().cast::<u8>().add(leading_zeros),
-                16 - leading_zeros,
-            ))
-        });
+        let bcd_hi = to_bcd8(bbccddee);
+        let leading_zeros = bcd_hi.leading_zeros() as usize / 8;
+        let bcd_lo = to_bcd8(ffgghhii);
+        let bytes = [
+            (bcd_hi | ZEROS).to_be_bytes(),
+            (bcd_lo | ZEROS).to_be_bytes(),
+        ];
+        f(unsafe { str::from_utf8_unchecked(&bytes.as_flattened()[leading_zeros..]) });
     } else {
         f(itoa::Buffer::new().format(value));
     }
